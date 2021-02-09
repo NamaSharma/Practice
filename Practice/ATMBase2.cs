44f06f3bb8c67da38243f584233dc54ff42cff95
Nilesh Upadhyay <upadhyaynilesh55@gmail.com>
2021-02-09 11:19:04 +0530

    ok

--- Practice/ATMBase2.cs
+++ Practice/ATMBase2.cs
@@ -1,14 +1,16 @@
-﻿namespace ATmV2
+﻿using DataAccess;
+
+namespace ATmV2
 {
     public class ATMBase2
     {
 
-        public bool AddNewAccount(long cardNumber, int pin)
+        public bool AddNewAccount(long cardNumber,string name, int pin, long currentBalance)
         {
             ATM atmObject = null;
             using (var dbContext = new ATMEntities())
             {
-                var results = dbContext.spNewAccount(cardNumber, pin).GetEnumerator();
+                var results = dbContext.spNewAccount(cardNumber,name, pin,currentBalance).GetEnumerator();
                 while (results.MoveNext())
                 {
                     var result = results.Current;
@@ -20,7 +22,7 @@
                     //atmObject.Cvv = result.Cvv;
                     atmObject.CurrentBalance = result.CurrentBalance.Value;
                 }
-
+                return true;
             }
         }
     }
